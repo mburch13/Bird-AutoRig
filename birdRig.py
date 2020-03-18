846344d2bb86c01f9503e4caec1c5a0d1736b23a
McKenzie <46796707+mburch13@users.noreply.github.com>
2020-03-18 16:56:26 -0500

    Update GUI and add Animation button

--- birdRig.py
+++ birdRig.py
@@ -1,7 +1,9 @@
 import maya.cmds as cmds
 import birdLoc as loc
+import birdAnim as anim
 
 loc = reload(loc)
+anim = reload(anim)
 
 class Rig():
     def __init__(self):
@@ -14,11 +16,12 @@ class Rig():
         cmds.button('Locators', w=200, c='loc.locators()')
         cmds.button('Rig', w=200, c='loc.createRig()')
         cmds.button('Bind', w=200, c='loc.BindSkin()')
+        cmds.button('Animate', w=200, c='anim.birdAnim()')
         cmds.button('Clear', w=200, c=self.clearScene)
         cmds.showWindow()
         
     def clearScene(self, void):
-		cmds.delete('Locators')
+		cmds.delete('loc*')
 		cmds.delete('Joints')
 		cmds.delete('Ctrl*')
 		print 'Scene Clearded'
