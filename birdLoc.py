378f1306ed02d9102a682461b88af4b9b7dbb5bf
McKenzie <46796707+mburch13@users.noreply.github.com>
2020-04-02 10:20:48 -0500

    Added more locator positions for leg and spine positions

--- birdLoc.py
+++ birdLoc.py
@@ -3,66 +3,57 @@
 import maya.cmds as cmds
 
 def locators():
-    locGrp = cmds.group(n='Locators_Left', em=True)
-    root = cmds.spaceLocator(n='loc_0')
+    root = cmds.spaceLocator(n='loc_root')
+    cmds.move(0,1.5,-.5)
+    cmds.scale(.5,.5,.5)
+
+    body = cmds.spaceLocator(n='loc_spine_body')
+    cmds.move(0,2,0)
+    cmds.scale(.5,.5,.5)
+    cmds.parent(body, root)
+    
+    wingCon = cmds.spaceLocator(n='loc_spine_wing_connect')
     cmds.move(0,3,0)
     cmds.scale(.5,.5,.5)
+    cmds.parent(wingCon, root)
+    
+    locGrp = cmds.group(n='Locators_Left_Wing', em=True)
     cmds.parent(locGrp, root)
     for i in range(6):
-        l = cmds.spaceLocator(n='loc_'+str(i+1))
+        l = cmds.spaceLocator(n='loc_wing_'+str(i+1))
         cmds.move(.4+(i/3.0), 3+(i/9.0), i/10.0)
         cmds.scale(.2,.2,.2)
         cmds.parent(l, locGrp)
-    tip=cmds.spaceLocator(n='loc_7')
+    tip=cmds.spaceLocator(n='loc_wing_tip')
     cmds.move(4.6, 2.6, 0)
-    cmds.parent(tip, locGrp)
+    cmds.parent(tip, locGrp)    
+    
     cmds.select(locGrp)
-    cmds.duplicate(n='Locators_Right',rr=True, rc=True)
+    cmds.duplicate(n='Locators_Right_Wing',rr=True, rc=False)
+    cmds.scale(-2,2,2)
+    
+    
+    
+    
+    locGrp2 = cmds.group(n='Locators_Left_Leg', em=True)
+    cmds.parent(locGrp2, root)
+    leg = cmds.spaceLocator(n='loc_leg')
+    cmds.move(.42,1.15,-.35)    
+    cmds.scale(.5,.5,.5)
+    cmds.parent(leg, locGrp2)
+    
+    knee = cmds.spaceLocator(n='loc_leg_knee')
+    cmds.move(.5,.57,-.15)
+    cmds.scale(.5,.5,.5)
+    cmds.parent(knee, locGrp2)
+ 
+    ankle = cmds.spaceLocator(n='loc_leg_ankle')
+    cmds.move(.5,.16,.12)
+    cmds.scale(.5,.5,.5)
+    cmds.parent(ankle, locGrp2)
+    
+    cmds.select(locGrp2)
+    cmds.duplicate(n='Locators_Right_Leg',rr=True, rc=True)
     cmds.scale(-2,2,2)
     print 'locators created'
-    
-def createRig():
-    master = cmds.circle(nr=(0,1,0), c=(0,0,0),  radius=2, name='Ctrl_Master')	
-    
-    jntGrp=cmds.group(n='Joints',em=True)
-    locs = cmds.ls('loc*', type='transform')
-    jnts = []
-    ctrls = []
-	
-    for i, lc in enumerate(locs):
-        lPos = cmds.xform(lc, q=True, t=True, ws=True)
-        j=cmds.joint(radius=.5, p=lPos, name='jnt_'+str(i))
-        jnts.append(j)
-        if i == 8:
-            cmds.parent(j, 'jnt_0')
-    
-    cmds.select('jnt_0')
-    cmds.joint(e=True, ch=True, oj='xyz', secondaryAxisOrient='yup')
-    
-    for i, j in enumerate(jnts):
-    	jPos = cmds.xform(j, q=True, t=True, ws=True)
-    	ctrl = cmds.circle(nr=(0,1,0), c=(0,0,0),  radius=1, name='ctrl_'+str(i))
-    	ctrls.append(ctrl)
-    	for c in ctrls:
-    		cmds.move(jPos[0],jPos[1],jPos[2])
-    		cmds.rotate(90,90,0)
-    	cmds.makeIdentity(apply=True, t=1, s=1)
-    	if i == 0:
-    		cmds.parent(ctrl, master)
-		
-    	else:
-    		cmds.parent(ctrl, 'ctrl_'+str(i-1))
-    		
-        if i == 8:
-		    cmds.parent(ctrl, 'ctrl_0')
-	cmds.parentConstraint(ctrl, j, mo=True)
-
-def BindSkin():
-	cmds.select('bird')
-	sel = cmds.ls(selection = True)
-	for i in range(0, len(sel)):
-		cmds.skinCluster(sel[i], 'jnt_0', bm=3, sm=1, dr=0.1, name='Mesh'+str(i))
-		cmds.geomBind('Mesh'+str(i), bm=3, gvp=[256,1], mi=1)
-	print 'Skin Bound'
-	
     
