b8f2e88f91809f5289980743f04bc4685446f564
McKenzie <46796707+mburch13@users.noreply.github.com>
2020-04-02 10:21:55 -0500

    Updated GUI for clarity and proper file pathways

--- birdRig.py
+++ birdRig.py
@@ -1,8 +1,10 @@
 import maya.cmds as cmds
 import birdLoc as loc
+import birdJoints as jnts
 import birdAnim as anim
 
 loc = reload(loc)
+jnts = reload(jnts)
 anim = reload(anim)
 
 class Rig():
@@ -13,17 +15,16 @@ class Rig():
         cmds.window("Bird Rig")
         cmds.rowColumnLayout(nc = 1, adjustableColumn = True)
 
-        cmds.button('Locators', w=200, c='loc.locators()')
-        cmds.button('Rig', w=200, c='loc.createRig()')
-        cmds.button('Bind', w=200, c='loc.BindSkin()')
-        cmds.button('Animate', w=200, c='anim.birdAnim()')
+        cmds.button('Create Locators', w=200, c='loc.locators()')
+        cmds.button('Create Rig', w=200, c='jnts.createRig()')
+        cmds.button('Bind Skin', w=200, c='jnts.BindSkin()')
+        cmds.button('Animate Character', w=200, c='anim.birdAnim()')
         cmds.button('Clear', w=200, c=self.clearScene)
         cmds.showWindow()
         
     def clearScene(self, void):
-		cmds.delete('loc*')
 		cmds.delete('Joints')
-		cmds.delete('Ctrl*')
+		cmds.delete('ctrl*')
 		print 'Scene Clearded'
 		
 Rig()
