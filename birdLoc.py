fa262fc15d9eccb4d7f3558c25836c1f9d5e3cbb
McKenzie <46796707+mburch13@users.noreply.github.com>
2020-04-05 16:46:53 -0500

    Fixed wing structure

--- birdLoc.py
+++ birdLoc.py
@@ -4,26 +4,36 @@ import maya.cmds as cmds
 
 def locators():
     root = cmds.spaceLocator(n='loc_root')
+    cmds.move(0,3,0)
+    cmds.scale(.5,.5,.5)
+    
+    hipCon = cmds.spaceLocator(n='loc_spine_hip_connect')
     cmds.move(0,1.5,-.5)
     cmds.scale(.5,.5,.5)
+    cmds.parent(hipCon, root)
 
     body = cmds.spaceLocator(n='loc_spine_body')
     cmds.move(0,2,0)
     cmds.scale(.5,.5,.5)
     cmds.parent(body, root)
     
-    wingCon = cmds.spaceLocator(n='loc_spine_wing_connect')
-    cmds.move(0,3,0)
-    cmds.scale(.5,.5,.5)
-    cmds.parent(wingCon, root)
-    
     locGrp = cmds.group(n='Locators_Left_Wing', em=True)
     cmds.parent(locGrp, root)
-    for i in range(6):
-        l = cmds.spaceLocator(n='loc_wing_'+str(i+1))
-        cmds.move(.4+(i/3.0), 3+(i/9.0), i/10.0)
-        cmds.scale(.2,.2,.2)
-        cmds.parent(l, locGrp)
+    shoulder = cmds.spaceLocator(n='loc_wing_shoulder')
+    cmds.move(.7, 3.1, 0.05)
+    cmds.scale(.2,.2,.2)
+    cmds.parent(shoulder, locGrp)
+    
+    elbow=cmds.spaceLocator(n='loc_wing_elbow')
+    cmds.move(1.4, 1.7, -.7)
+    cmds.scale(.2,.2,.2)
+    cmds.parent(elbow, locGrp)
+    
+    mid=cmds.spaceLocator(n='loc_wing_mid')
+    cmds.move(2.2, 3.5, .46)
+    cmds.scale(.2,.2,.2)
+    cmds.parent(mid, locGrp)
+    
     tip=cmds.spaceLocator(n='loc_wing_tip')
     cmds.move(4.6, 2.6, 0)
     cmds.parent(tip, locGrp)    
@@ -31,9 +41,6 @@ def locators():
     cmds.select(locGrp)
     cmds.duplicate(n='Locators_Right_Wing',rr=True, rc=False)
     cmds.scale(-2,2,2)
-    
-    
-    
     
     locGrp2 = cmds.group(n='Locators_Left_Leg', em=True)
     cmds.parent(locGrp2, root)
